4f888a40a32bd72792c26688ba6db3b6f438ed14
Olivia Burge <olivia.burge@gmail.com>
2017-06-21 11:08:16 +1200

    more changes for wgs84 - going from epsg 3857 to epsg 4326

--- R/find_my_island.R
+++ R/find_my_island.R
@@ -20,7 +20,7 @@
 
 find_my_island <- function(islandname, proj = "wgs84"){
 
-  wgs84 <- CRS("+init=epsg:3857 +proj=longlat ")
+  wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
   nztm <- CRS("+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ")
 
 
--- R/fortifying_polygons.R
+++ R/fortifying_polygons.R
@@ -11,18 +11,18 @@
 fortify_polygons <- function(shape, proj){
 
   nztm <- CRS("+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ")
-  wgs84 <- CRS("+init=epsg:3857 +proj=longlat ")
+  wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
 
   if(class(shape) != "SpatialPolygonsDataFrame"){
     stop("function only to be used for SpatialPolygonsDataFrame, use fortify_points() for points")
   }
 
-  if(proj == "NZTM"){
+  if(proj %in% c("NZTM", "nztm")){
     shapell <- spTransform(shape, CRSobj = nztm)
-  } else if(proj == "WGS84"){
+  } else if(proj %in% c("WGS84", "wgs84")){
     shapell <- spTransform(shape, CRSobj = wgs84)
   } else{
-    stop("Only NZTM and WGS84 projections currently allowed. Submit a pull request or issue if you want another one")
+    stop("Only nztm and wgs84 projections currently allowed. Submit a pull request or issue if you want another one")
   }
   shapell@data$id <- rownames(shapell@data)
   fortified <- tidy(shapell, region = "id")
